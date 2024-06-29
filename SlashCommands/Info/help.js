5c00b9139eadad85f016d84b23e546860d1c9346
yehia sakr <160256331+yehiaSAKR29@users.noreply.github.com>
2024-06-29 15:39:31 +0300

    Updated help.js
    added a new command

--- SlashCommands/Info/help.js
+++ SlashCommands/Info/help.js
@@ -9,7 +9,7 @@ module.exports = {
     const helpEmbed = {
       color: 0xff0000,
       title: '__COMMANDS LIST__',
-      description: '**/8ball**\nAsks the magic 8ball a question\n\n**/coinflip**\nFlips a coin\n\n**/kill**\nKills someone\n\n**/poll**\nCreates a poll\n\n**/purge**\nPurges messages in the channel\n\n**/rps**\nChallenges someone to a duel of rock paper scissors\n\n**/say**\nSays something',
+      description: '**/8ball**\nAsks the magic 8ball a question\n\n**/ban**\nBans someone from this server\n\n**/coinflip**\nFlips a coin\n\n**/kill**\nKills someone\n\n**/poll**\nCreates a poll\n\n**/purge**\nPurges messages in the channel\n\n**/rps**\nChallenges someone to a duel of rock paper scissors\n\n**/say**\nSays something',
       timestamp: new Date(),
       footer: {
         icon_url: interaction.user.avatarURL(),
